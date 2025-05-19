477005a04357eb549c9767aa1890ed1ddad23314
rohmanhida <hodyjones99@gmail.com>
2025-05-19 17:44:41 +0700

    add netlify domain

--- handlers/muxH.go
+++ handlers/muxH.go
@@ -9,9 +9,8 @@ import (
 var Mux = http.NewServeMux()
 
 func MuxHandler() http.Handler {
-
 	handler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173"}, // Svelte dev server
+		AllowedOrigins:   []string{"http://localhost:5173", "http://chat-sv.netlify.app"}, // Svelte dev server
 		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type"},
 		AllowCredentials: true,
